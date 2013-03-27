afadfe600662fe1cb378efdd911acabcc2f5db63
Nekuromento <max.klyga@gmail.com>
2013-03-28 01:55:39 +0300

    Use reducers to utilize more cores

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,15 +1,20 @@
 (ns clojure-course-task02.core
+  (:require [clojure.core.reducers :as r])
   (:gen-class))
 
 
 (defn find-files [file-name path]
   (let [pattern (re-pattern file-name)
         files (file-seq (clojure.java.io/file path))]
-    (filter #(not (nil? %))
-            (map #(re-find pattern (.getName %)) files))))
+    (r/filter #(not (nil? %))
+              (r/map #(re-find pattern (.getName %)) files))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
+
+(defn print-file
+  ([] nil)
+  ([_ file-name] (println file-name)))
 
 (defn -main [file-name path]
   (if (or (nil? file-name)
@@ -17,4 +22,4 @@
     (usage)
     (do
       (println "Searching for " file-name " in " path "...")
-      (dorun (map println (find-files file-name path))))))
+      (r/fold print-file (find-files file-name path)))))
--- test/clojure_course_task02/core_test.clj
+++ test/clojure_course_task02/core_test.clj
@@ -4,11 +4,11 @@
 
 (deftest find-files-test
   (testing "find-files count testing..."
-    (is (<= 2 (count (find-files "^core.+" "./"))))))
+    (is (<= 2 (count (into [] (find-files "^core.+" "./")))))))
 
 
 (deftest find-files-test2
   (testing "find-files contents testing..."
-    (let [res (apply hash-set (find-files "^core.+" "./"))]
+    (let [res (into #{} (find-files "^core.+" "./"))]
       (is (contains? res "core.clj"))
       (is (contains? res "core_test.clj")))))
