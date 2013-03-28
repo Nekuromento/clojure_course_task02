157eb9c97e7b9951384191ec06372fc7ff6c9a5f
Nekuromento <max.klyga@gmail.com>
2013-03-28 05:28:20 +0300

    Implement file-seq with reducers. Enable warning on reflection. Add type hints to remove reflection calls.

--- src/clojure_course_task02/core.clj
+++ src/clojure_course_task02/core.clj
@@ -1,20 +1,32 @@
 (ns clojure-course-task02.core
   (:require [clojure.core.reducers :as r])
+  (:import java.io.File)
   (:gen-class))
 
+(set! *warn-on-reflection* true)
+
+(defn file-seq-r [file]
+  (let [content (.listFiles ^File file) ;on big folders this can cause out of memory error
+        files (filter (memfn ^File isFile) content)
+        dirs (r/filter (memfn ^File isDirectory) content)
+        dirs-content (r/foldcat (r/mapcat file-seq-r dirs))]
+    (r/cat files dirs-content)))
 
 (defn find-files [file-name path]
   (let [pattern (re-pattern file-name)
-        files (file-seq (clojure.java.io/file path))]
+        files (file-seq-r (clojure.java.io/file path))]
     (r/filter #(not (nil? %))
-              (r/map #(re-find pattern (.getName %)) files))))
+              (r/map #(re-find pattern (.getName ^File %)) files))))
 
 (defn usage []
   (println "Usage: $ run.sh file_name path"))
 
 (defn print-file
-  ([] nil)
-  ([_ file-name] (println file-name)))
+  ([]
+   nil)
+  ([_ file-name]
+   (if (not (nil? file-name))
+     (println file-name))))
 
 (defn -main [file-name path]
   (if (or (nil? file-name)
